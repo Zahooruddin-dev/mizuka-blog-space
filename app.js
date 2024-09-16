51efd7f758b2ec06ad03fdad9f1fd96551a6969f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-17 04:06:07 +0500

    post working in log

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const url = 'https://apis.scrimba.com/jsonplaceholder/posts';
-
 	fetch(url, { method: 'GET' })
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data);
+			
 
 			const postArr = data.slice(0, 3); // Adjust as needed
 			let html = '';
@@ -21,28 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
 			document.getElementById('container').innerHTML = html;
 		})
 		.catch((error) => {
-			console.error('Error fetching data:', error);
+			console.error('error fetching the data:', error);
 			document.getElementById('container').innerHTML =
-				'<p>Failed to load content. Please try again later.</p>';
+				'<h1>Please try again Failedto load content</h1>';
 		});
-
+	
 	document.querySelector('form').addEventListener('submit', (event) => {
-		event.preventDefault(); 
-
-		const title = document.getElementById('title-text').value;
-		const body = document.getElementById('content-text').value;
-
-		console.log('Title:', title);
-		console.log('Body:', body);
+		event.preventDefault();
+		const postTitle = document.getElementById('title-text').value;
+		const postBody = document.getElementById('content-text').value;
+		const dataObject = {
+			title: postTitle,
+			body: postBody,
+		};
+		console.log(dataObject);
 	});
 });
-
-/**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
-
-*/
